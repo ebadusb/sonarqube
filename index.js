27c6b922b37e644438c0e228d1091c63ac766b14
Eric Badiere <eric.badiere@terumobct.com>
2021-05-18 09:06:24 -0600

    Added exec and git ignore.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+ 
+  await exec.exec('start-sonarqube.ps1');
+ 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
