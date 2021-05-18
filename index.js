24098502e202d16c5862b6fd97ecb96a7ee9269e
Eric Badiere <eric.badiere@terumobct.com>
2021-05-18 10:10:01 -0600

    Next it.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const scan = async () => {
     console.log("Debug: About to start scan");
     // await io.cp('start-sonarqube.ps1', '../start-sonarqube.ps1');
     await exec.exec('ls ../');
-    // await exec.exec('start-sonarqube.ps1');
+    await exec.exec('start-sonarqube.ps1');
 }
 
 try {
